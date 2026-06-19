8a245495650fc467a994ed5d10c2a77a6b06f112
crafter23456 <64259198+crafter23456@users.noreply.github.com>
2026-06-19 19:24:42 +0200

    add todo

--- src/jaxatari/games/jax_demonattack.py
+++ src/jaxatari/games/jax_demonattack.py
@@ -245,7 +245,7 @@ class DemonAttackConstants(struct.PyTreeNode):
         pytree_node=False,
         default=(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)
     )
-    WAVE_LASER_SPEED_TABLE: Tuple[int, ...] = struct.field(pytree_node=False, default=(1.5, 1, 2, 2, 3, 3))
+    WAVE_LASER_SPEED_TABLE: Tuple[int, ...] = struct.field(pytree_node=False, default=(1.5, 1.5, 2, 2, 3, 3)) # TODO needs adjustments
     ENEMY_SHOT_ACTION_TABLE: Tuple[int, ...] = struct.field(
         pytree_node=False,
         default=(8, 6, 6, 3, 5, 4, 5, 4, 5, 4, 5, 4),
